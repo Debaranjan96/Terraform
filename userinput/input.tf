94b7d93d8dcdb5aff90b415a92ce8f8ac2f43335
Debaranjan96 <89602120+Debaranjan96@users.noreply.github.com>
2023-06-25 23:32:06 +0530

    list variable example

--- userinput/input.tf
+++ userinput/input.tf
@@ -2,7 +2,11 @@
 output "printname" {
   //value = "Hello var.varname"-------> it will print "hello var.varname"
 
-  value = "hello, ${var.username}"
+  value = "hello  ${var.username} ,your age is ${var.age}"
 
   //terraform plan -var "Dev"---> It will take direct input during command execution.
+}
+
+output "printlist" {
+  value = "First product is ${var.products[0]}"
 }
--- userinput/variable.tf
+++ userinput/variable.tf
@@ -1,3 +1,13 @@
 variable "username" {
-  
+  type = string
+  //default = "Dev"---->it will print default value automatically
+}
+
+variable "age" {
+  type = number
+}
+
+variable "products" {
+  type = list(number)
+
 }
