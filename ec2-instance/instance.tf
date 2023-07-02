588e7c3fd744a015aa62921e74555443bd2ab7af
Debaranjan96 <89602120+Debaranjan96@users.noreply.github.com>
2023-07-02 19:55:05 +0530

    ec2 instance example

---
+++ ec2-instance/instance.tf
@@ -0,0 +1,19 @@
+# creating key in aws
+resource "aws_key_pair" "key-tf" {
+  key_name   = "first-key"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+output "key-name" {
+  value = aws_key_pair.key-tf.key_name
+}
+
+
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-06b09bfacae1453cb"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "terraforminstance"
+#   }
+# }
---
+++ ec2-instance/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAQY66A42BQ3EMBNGI"
+  secret_key = "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"
+}
