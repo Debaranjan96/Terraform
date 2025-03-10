6a8309bcc6ea6674995ee007a4cdcf4e40364ac9
Debaranjan96 <89602120+Debaranjan96@users.noreply.github.com>
2025-03-10 19:08:40 +0530

    updates

--- ec2-instance/instance.tf
+++ ec2-instance/instance.tf
@@ -10,10 +10,10 @@ output "key-name" {
 
 
 
-# resource "aws_instance" "web" {
-#   ami           = "ami-06b09bfacae1453cb"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "terraforminstance"
-#   }
-# }
+resource "aws_instance" "web" {
+  ami           = "ami-06b09bfacae1453cb"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraforminstance"
+  }
+}
